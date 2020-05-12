50a0c87f9c798d1c7b5cb8b8799475eefc2c2136
Vedran Kasalica <v.kasalica@uu.nl>
2020-05-13 01:59:33 +0200

    Fixing .gitignore and adjusting UseCases for APE v1.0

--- GeoGMT/E1/Implementations/run.sh
+++ GeoGMT/E1/Implementations/run.sh
@@ -3,21 +3,8 @@ unset noclobber
 
 sudo chmod u+x workflow*
 
-./workflowSolution_0.sh
+./workflowSolution_0.sh move.txt
 echo "Workflow 0 executed"
-
-./workflowSolution_1.sh
-echo "Workflow 1 executed"
-
-./workflowSolution_2.sh
-echo "Workflow 2 executed"
-
-./workflowSolution_3.sh
-echo "Workflow 3 executed"
-
-./workflowSolution_4.sh
-echo "Workflow 4 executed"
-
 
 
 
